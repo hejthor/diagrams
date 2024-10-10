277e7df59b98295e1dc2e5b60cccc9813a08c0af
Thor ✨ <hello@thor.computer>
2024-10-10 21:59:30 +0200

    Delete compile.bat

--- compile.bat
+++
@@ -1,5 +0,0 @@
-java -jar "resources/plantuml-1.2024.7.jar" -charset UTF-8 -tpng "input/*.puml" -o "../output"
-java -jar "resources/plantuml-1.2024.7.jar" -charset UTF-8 -tsvg "input/*.puml" -o "../output"
-:: java -jar "resources/plantuml-1.2024.7.jar" -charset UTF-8 -tvdx "input/*.puml" -o "../output"
-:: java -jar "resources/plantuml-1.2024.7.jar" -charset UTF-8 -preproc "input/*.puml" -o "../output"
-:: java -jar "resources/plantuml-1.2024.7.jar" -charset UTF-8 -computeurl "input/*.puml" -o "../output"
